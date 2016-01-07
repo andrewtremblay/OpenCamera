22175a06f0c34b235a1e7a0e77fec91ba86639c8
Andrew Tremblay <him@andrew-tremblay.com>
2016-01-07 10:26:14 -0500

    initial commit

---
+++ app/src/main/java/com/andrewtremblayllc/opencam/MainCameraActivity.java
@@ -0,0 +1,13 @@
+package com.andrewtremblayllc.opencam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_camera);
+    }
+}
